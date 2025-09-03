995cf143377325c9572709a72ed69c7886535eb9
Matt Mercieca <mercieca@gmail.com>
2025-09-03 14:32:56 -0400

    Adding specs for HTTP service

--- app/services/http_service.rb
+++ app/services/http_service.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+class CommunicationError < StandardError; end
+
 class HttpService
-  def initialize(url:, method: 'GET', body: nil)
+  def initialize(url:, method: "GET", body: nil)
     parse_uri(url)
     raise ArgumentError, I18n.t("http_service.invalid_url") if @port.nil? || @host.nil? || @scheme.nil?
+    rails ArgumentError, I18n.t("http_service.invalid_url") if @scheme != "http" && @scheme != "https"
 
     @method = method
     @body = nil
@@ -17,12 +20,45 @@ class HttpService
     @body
   end
 
+  def host
+    @host
+  end
+
+  def port
+    @port
+  end
+
+  def uri
+    @uri
+  end
+
+  def scheme
+    @scheme
+  end
+
   def parse_uri(url)
     @uri = URI.parse(url)
     @port = @uri.port
     @host = @uri.host
     @path = @uri.path
     @scheme = @uri.scheme
+  end
+
+  def get
+    http = Net::HTTP.new(host, port)
+    http.use_ssl = (scheme == "https")
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    begin
+      response = http.request(request)
+      return response.body if response.code == "200"
+    rescue
+      # We could be better about this later but time is short.  For this project it really doesn't matter
+      # why we can't reach the weather service, just that we can't.
+    end
+
+    raise CommunicationError
   end
 
   def exec
--- spec/services/http_service_spec.rb
+++ spec/services/http_service_spec.rb
@@ -13,11 +13,72 @@ RSpec.describe HttpService, type: :service do
 
       expect(http.method).to eq('GET')
     end
-  
+
     it 'does not require a body' do
       http = described_class.new(url: 'https://www.example.com')
 
       expect(http.body).to be_nil
+    end
+  end
+
+  context 'when posting' do
+    it 'throws an exception' do
+      expect { HttpService.new('https://www.example.com', method: 'POST') }.to raise_error(ArgumentError)
+    end
+  end
+
+  context 'when getting' do
+    let(:url) { 'https://www.example.com/' }
+    let(:uri) { URI.parse(url) }
+    let(:mock_http) { instance_double(Net::HTTP) }
+    let(:mock_request) { instance_double(Net::HTTP::Get) }
+    let(:body) { 'This is the body' }
+
+    before do
+      allow(Net::HTTP).to receive(:new).with(uri.host, uri.port).and_return(mock_http)
+      allow(mock_http).to receive(:use_ssl=)
+      allow(Net::HTTP::Get).to receive(:new).with(uri.request_uri).and_return(mock_request)
+    end
+
+    context 'when successful' do
+      let(:mock_response) { instance_double(Net::HTTPResponse, body: body, code: "200") }
+
+      before do
+        allow(mock_http).to receive(:request).with(mock_request).and_return(mock_response)
+        allow(mock_response).to receive(:code).and_return("200")
+      end
+
+      it 'returns the body when successful' do
+        http = described_class.new(url: 'https://www.example.com')
+        text = http.get
+
+        expect(text).to eq(body)
+      end
+    end
+
+    context 'with errors' do
+      context 'when the document has moved' do
+        let(:mock_response) { instance_double(Net::HTTPResponse, body: body, code: "301") }
+
+        before do
+          allow(mock_http).to receive(:request).with(mock_request).and_return(mock_response)
+          allow(mock_response).to receive(:code).and_return("301")
+        end
+
+        it 'throws a CommunicationError' do
+          expect { HttpService.new(url: 'https://www.example.com').get }.to raise_error(CommunicationError)
+        end
+      end
+
+      context 'when there is an error using Net::HTTP' do
+        before do
+          allow(mock_http).to receive(:request).and_raise(Errno::ECONNREFUSED)
+        end
+
+        it 'throws a CommunicationError' do
+          expect { HttpService.new(url: 'https://www.example.com').get }.to raise_error(CommunicationError)
+        end
+      end
     end
   end
 end
